960a344d426f2a0bae310b104f9759f028711e70
gatperdut <carlos.romero.breto@gmail.com>
2026-04-02 15:04:41 +0200

    stuff

--- hwbe/src/app.service.ts
+++ hwbe/src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello() {
     return { greeting: 'Hello World!' };
   }
+
+  test() {
+        const a = 'shadowfax';
+
+    const b = 'djif';
+
+    return a == 'shadowfax';
+  }
 }
