2ce962c29337f943467381e6f65a2f9ea398feda
gatperdut <carlos.romero.breto@gmail.com>
2026-04-02 13:48:34 +0200

    sjdifd

--- hwbe/src/main.ts
+++ hwbe/src/main.ts
@@ -1,8 +1,13 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap(): Promise<void> {
+  const app: INestApplication<any> = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
