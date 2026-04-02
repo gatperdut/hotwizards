cf4b6de34ecb6b6d86c3e26ca9405c2c64b27c8e
gatperdut <carlos.romero.breto@gmail.com>
2026-04-02 13:57:11 +0200

    hwbe-api => hwbe

--- hwbe/src/app.service.ts
+++ hwbe/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return { greeting: 'Hello World!!!' };
+    return { greeting: 'Hello World!' };
   }
 }
