d1766a1c5b4dba10a929ba937f0892c67c584ca5
gatperdut <carlos.romero.breto@gmail.com>
2026-04-05 16:31:30 +0200

    sdifjisdf

--- hwbe/prisma.config.ts
+++ hwbe/prisma.config.ts
@@ -1,13 +1,7 @@
 import 'dotenv/config';
 import type { PrismaConfig } from 'prisma';
-import { env } from 'prisma/config';
 
-console.log(process.env);
-console.log('THIS ONE', process.env['HWBE_DB_URL']);
-
-console.log('env obj', env('HWBE_DB_URL'));
-
-export default {
+const config: PrismaConfig = {
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
@@ -16,4 +10,8 @@ export default {
   datasource: {
     url: process.env['HWBE_DB_URL'],
   },
-} satisfies PrismaConfig;
+};
+
+console.log(config);
+
+export default config satisfies PrismaConfig;
