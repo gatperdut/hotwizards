9f8967e975f9af0cd9cca20ac51a8cd6c482eaca
gatperdut <carlos.romero.breto@gmail.com>
2026-04-06 06:54:43 +0200

    progress

--- hwbe/src/prisma/prisma.service.ts
+++ hwbe/src/prisma/prisma.service.ts
@@ -1,7 +1,7 @@
+import { PrismaClient } from '#generated/client.js';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from 'generated/client.js';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
