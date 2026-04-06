539affc933fe8c939a303e4b01fbba958336b77b
gatperdut <carlos.romero.breto@gmail.com>
2026-04-06 14:03:58 +0200

    try building before pushing

--- hwfe/src/environments/environment.ts
+++ hwfe/src/environments/environment.ts
@@ -2,6 +2,6 @@ import { AppEnvironment } from '@hw/hwfe/environments/environment.model.js';
 
 export const environment: AppEnvironment = {
   production: true,
-  sentryDsn: 'HWFE_SENTRY_DSN',
-  hwfeSentryRelease: 'HWFE_SENTRY_RELEASE',
+  sentryDsn: 'HWFE_SENTRY_DSN_PLACEHOLDER',
+  hwfeSentryRelease: 'HWFE_SENTRY_RELEASE_PLACEHOLDER',
 };
