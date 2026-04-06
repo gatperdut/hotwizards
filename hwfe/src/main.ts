60e4934c0a3111ac6c151c6efa651c663057eb07
gatperdut <carlos.romero.breto@gmail.com>
2026-04-06 16:35:55 +0200

    localhost and shit

--- hwfe/src/main.ts
+++ hwfe/src/main.ts
@@ -4,22 +4,21 @@ import { appConfig } from './app/app.config.js';
 import { environment } from './environments/environment.js';
 
 if (environment.production) {
-  void import('@sentry/angular').then((Sentry) => {
+  void import('@sentry/angular').then((Sentry): void => {
     Sentry.init({
       dsn: environment.sentryDsn,
       release: environment.hwfeSentryRelease,
       integrations: [Sentry.browserTracingIntegration()],
       tracesSampleRate: 0.1,
-      // TODO localhost?
-      tracePropagationTargets: ['localhost', 'hotwizards.net'],
+      tracePropagationTargets: ['hotwizards.net'],
       replaysSessionSampleRate: 0.1,
       replaysOnErrorSampleRate: 1.0,
     });
 
-    setTimeout(async () => {
+    setTimeout(async (): Promise<void> => {
       Sentry.addIntegration(Sentry.replayIntegration());
     }, 5000);
   });
 }
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err): void => console.error(err));
