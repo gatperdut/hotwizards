a3ec3c5b65e9edd5ca395d0615a08af50f2fe8a3
gatperdut <carlos.romero.breto@gmail.com>
2026-04-06 16:34:00 +0200

    GET THE CHUNK DOWN

--- hwfe/src/main.ts
+++ hwfe/src/main.ts
@@ -1,25 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { browserTracingIntegration, init } from '@sentry/angular';
 import { AppComponent } from './app/app.component.js';
 import { appConfig } from './app/app.config.js';
 import { environment } from './environments/environment.js';
 
-// if (environment.production) {
-init({
-  dsn: environment.sentryDsn,
-  release: environment.hwfeSentryRelease,
-  integrations: [browserTracingIntegration()],
-  tracesSampleRate: 0.1,
-  tracePropagationTargets: ['localhost', 'hotwizards.net'],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+if (environment.production) {
+  void import('@sentry/angular').then((Sentry) => {
+    Sentry.init({
+      dsn: environment.sentryDsn,
+      release: environment.hwfeSentryRelease,
+      integrations: [Sentry.browserTracingIntegration()],
+      tracesSampleRate: 0.1,
+      // TODO localhost?
+      tracePropagationTargets: ['localhost', 'hotwizards.net'],
+      replaysSessionSampleRate: 0.1,
+      replaysOnErrorSampleRate: 1.0,
+    });
 
-setTimeout(async (): Promise<void> => {
-  const { addIntegration, replayIntegration } = await import('@sentry/angular');
-
-  addIntegration(replayIntegration());
-}, 5000);
-// }
+    setTimeout(async () => {
+      Sentry.addIntegration(Sentry.replayIntegration());
+    }, 5000);
+  });
+}
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
