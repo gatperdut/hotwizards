2e0ebf92c1952240ca370f2284910ff4fdd1f578
gatperdut <carlos.romero.breto@gmail.com>
2026-04-10 14:30:26 +0200

    of course.

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,5 +1,4 @@
-// TODO add index.ts in folders, which take care of the stuff inside them, and import only those here?
-export * from '../dist/shared/src/health/health-status.dto.js';
-export { AuthRegisterDto } from '../dist/shared/src/users/auth/auth-register.dto.js';
-export { type LoginDto } from '../dist/shared/src/users/auth/login.dto.js';
-export { type AppUser } from '../dist/shared/src/users/user.interface.js';
+export { HealthStatusDto } from './health/health-status.dto.js';
+export { AuthRegisterDto } from './users/auth/auth-register.dto.js';
+export type { LoginDto } from './users/auth/login.dto.js';
+export type { AppUser } from './users/user.interface.js';
