4ce3d9a67c3c0441d4709cb8b8b2747c4bbc32cd
gatperdut <carlos.romero.breto@gmail.com>
2026-04-12 10:14:43 +0200

    work on the toasts

--- hwfe/src/app/app.component.ts
+++ hwfe/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastContainerComponent } from './ui/toast/toast-container/toast-container.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
