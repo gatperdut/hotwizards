ea3a7fc52114e7cf1bef7db6bfa1143603f97203
gatperdut <carlos.romero.breto@gmail.com>
2026-04-18 17:56:08 +0200

    add cards, next invite (through modal?)

--- hwfe/src/app/campaigns/campaign/campaign.component.ts
+++ hwfe/src/app/campaigns/campaign/campaign.component.ts
@@ -1,13 +1,34 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, input, signal } from '@angular/core';
+import { AppCardAction, CardComponent } from '../../ui/card/card.component';
 import { MyCampaign } from '../types/my-campaign.type';
 
 @Component({
   selector: 'app-campaign',
-  imports: [],
+  imports: [CardComponent, NgTemplateOutlet],
   templateUrl: './campaign.component.html',
   styleUrl: './campaign.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CampaignComponent {
   public campaign = input.required<MyCampaign>();
+
+  public master = computed(() => this.campaign().master);
+
+  public activeMembers = computed(() =>
+    this.campaign().members.filter((member) => member.status === 'ACCEPTED'),
+  );
+
+  public pendingMembers = computed(() =>
+    this.campaign().members.filter((member) => member.status === 'PENDING'),
+  );
+
+  public actions = signal<AppCardAction[]>([
+    {
+      label: 'Invite',
+      action: (): void => {
+        console.log('Invite');
+      },
+    },
+  ]);
 }
---
+++ hwfe/src/app/ui/card/card.component.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+
+export type AppCardAction = {
+  label: string;
+  action: () => void;
+  color?: 'primary' | 'secondary' | 'warning';
+};
+
+@Component({
+  selector: 'app-card',
+  imports: [ButtonComponent],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CardComponent {
+  public title = input.required<string>();
+
+  public color = input<'primary' | 'secondary' | 'warning'>('primary');
+
+  public separatorColor = input<'primary' | 'secondary' | 'warning'>('secondary');
+
+  public actions = input<AppCardAction[]>([]);
+}
