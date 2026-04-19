d2ee8919bc820a400affba19a2444586b473ce1c
gatperdut <carlos.romero.breto@gmail.com>
2026-04-19 14:15:59 +0200

    working, need to polish

--- hwfe/src/app/ui/select/select.component.ts
+++ hwfe/src/app/ui/select/select.component.ts
@@ -1,94 +1,64 @@
-import { Component, computed, ElementRef, HostListener, input, signal } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, ElementRef, HostListener, input, model, output, signal } from '@angular/core';
 import { Field, form } from '@angular/forms/signals';
 import { InputTextComponent } from '../input-text/input-text.component';
 import { TagComponent } from '../tag/tag.component';
 
-export interface SelectOption {
-  label: string;
-  value: any;
-}
-
 @Component({
   selector: 'app-select',
   standalone: true,
-  imports: [InputTextComponent, TagComponent],
+  imports: [InputTextComponent, TagComponent, JsonPipe],
   templateUrl: './select.component.html',
   styleUrl: './select.component.css',
 })
 export class SelectComponent {
   public label = input.required<string>();
   public placeholder = input<string>('Select...');
-  public options = input<SelectOption[]>([]);
+  public options = input.required<any[] | undefined>();
+  public trackFn = input.required<(item: any) => string | number>();
+  public displayFn = input.required<(item: any) => string>();
   public field = input.required<Field<any>>();
+  public searchField = model.required<{ term: string }>();
   public multiple = input<boolean>(false);
   public loading = input<boolean>(false);
-  public searchable = input<boolean>(true);
+  public searchable = input<boolean>(true); // = equivalent to presence of searchField
+  public search = output<string>();
 
   constructor(private eRef: ElementRef) {}
 
   public id = `app-select-${Math.random().toString(36).substring(2, 9)}`;
   public isOpen = signal(false);
-  private model = signal({
-    term: '',
-  });
-  public form = form(this.model);
 
-  public filteredOptions = computed(() => {
-    const term = this.model().term;
-    const opts = this.options();
-
-    if (!term) {
-      return opts;
-    }
-    return opts.filter((o) => o.label.toLowerCase().includes(term));
-  });
-
-  public currentValue = computed(() => this.field()().value());
-
-  public displayText = computed(() => {
-    const val = this.currentValue();
-    const opts = this.options();
-
-    if (this.multiple()) {
-      return (
-        opts
-          .filter((o) => val.includes(o.value))
-          .map((o) => o.label)
-          .join(', ') || this.placeholder()
-      );
-    }
-
-    return opts.find((o) => o.value === val)?.label || this.placeholder();
-  });
+  public form = form(this.searchField);
 
   public toggle(): void {
     if (this.field()().disabled()) {
       return;
     }
 
-    if (this.isOpen()) {
-      this.model.set({ term: '' });
-    }
+    this.searchField.set({ term: '' });
 
     this.isOpen.update((v) => !v);
 
     this.field()().markAsTouched();
   }
 
-  public select(option: SelectOption): void {
+  public select(option: any): void {
     if (this.multiple()) {
-      const currentValue = this.currentValue();
+      const currentValue: any[] = this.field()().value();
 
-      const index = currentValue.indexOf(option.value);
+      const index = currentValue.findIndex(
+        (someOption) => this.trackFn()(option) === this.trackFn()(someOption),
+      );
       if (index >= 0) {
         currentValue.splice(index, 1);
       } else {
-        currentValue.push(option.value);
+        currentValue.push(option);
       }
 
       this.field()().value.set([...currentValue]);
     } else {
-      this.field()().value.set(option.value);
+      this.field()().value.set(option);
       this.isOpen.set(false);
       this.field()().markAsTouched();
     }
