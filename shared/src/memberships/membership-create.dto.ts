2cda15a46639ef55cadadb86788b8ba7a6c12a40
gatperdut <carlos.romero.breto@gmail.com>
2026-04-20 18:36:23 +0200

    work on joining a campaign

--- shared/src/memberships/membership-create.dto.ts
+++ shared/src/memberships/membership-create.dto.ts
@@ -1,8 +1,8 @@
-import { HwOwnedCampaignDto, ToArray } from '@hw/shared';
+import { HwCampaignPartialDto, ToArray } from '@hw/shared';
 import { Type } from 'class-transformer';
 import { ArrayNotEmpty, IsArray, IsInt, IsPositive } from 'class-validator';
 
-export class HwMembershipCreateDto extends HwOwnedCampaignDto {
+export class HwMembershipCreateDto extends HwCampaignPartialDto {
   @ToArray()
   @IsArray()
   @ArrayNotEmpty()
