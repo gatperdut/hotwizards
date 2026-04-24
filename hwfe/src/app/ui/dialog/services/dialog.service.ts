800246569e6eb1a7377dc98fe310e9945fc29b31
gatperdut <carlos.romero.breto@gmail.com>
2026-04-24 08:00:24 +0200

    lazy loaded dialogs

--- hwfe/src/app/ui/dialog/services/dialog.service.ts
+++ hwfe/src/app/ui/dialog/services/dialog.service.ts
@@ -11,12 +11,23 @@ import { DialogRef } from '../dialog-ref.class';
 
 export const APP_DIALOG_DATA = new InjectionToken<any>('APP_DIALOG_DATA');
 
+export interface LazyDialog<C, D, R> {
+  importFn: () => Promise<Type<C>>;
+  data?: D;
+  result?: R;
+}
+
 @Injectable({ providedIn: 'root' })
 export class DialogService {
   private appRef = inject(ApplicationRef);
   private injector = inject(Injector);
 
-  public open<T, D = any, R = any>(component: Type<T>, data: D): DialogRef<R> {
+  public async open<C, D, R>(lazy: LazyDialog<C, D, R>, data: D): Promise<DialogRef<R>> {
+    const component = await lazy.importFn();
+    return this.openInternal(component, data);
+  }
+
+  private openInternal<T, D = any, R = any>(component: Type<T>, data: D): DialogRef<R> {
     const dialogRef = new DialogRef<R>();
 
     const componentRef = createComponent(component, {
