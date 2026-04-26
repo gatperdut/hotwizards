4828e1dcd395cec951638827bac8912294e8ed2d
gatperdut <carlos.romero.breto@gmail.com>
2026-04-26 17:43:14 +0200

    stuff

--- hwbe/src/@types/socket.io.d.ts
+++ hwbe/src/@types/socket.io.d.ts
@@ -1,9 +1,7 @@
-// socket-data.d.ts
-
 import { HwUser } from '@hw/shared';
 
 declare module 'socket.io' {
-  interface Socket<ListenEvents, EmitEvents, ServerSideEvents, SocketData> {
+  interface Socket {
     user: HwUser;
   }
 }
