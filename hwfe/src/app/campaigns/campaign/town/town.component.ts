9ef22e3c60f0872466a63e79331ee6733f632647
gatperdut <carlos.romero.breto@gmail.com>
2026-05-01 12:38:57 +0200

    idsjf

--- hwfe/src/app/campaigns/campaign/town/town.component.ts
+++ hwfe/src/app/campaigns/campaign/town/town.component.ts
@@ -1,4 +1,3 @@
-import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ButtonComponent } from '@hw/hwfe/app/ui/button/button.component';
@@ -7,13 +6,13 @@ import { TownMembershipComponent } from './town-membership/town-membership.compo
 
 @Component({
   selector: 'app-town',
-  imports: [ButtonComponent, TownMembershipComponent, JsonPipe],
+  imports: [ButtonComponent, TownMembershipComponent],
   templateUrl: './town.component.html',
   styleUrl: './town.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TownComponent {
-  private campaignService = inject(CampaignService);
+  public campaignService = inject(CampaignService);
   private router = inject(Router);
 
   public memberships = computed(() => this.campaignService.campaign().memberships);
