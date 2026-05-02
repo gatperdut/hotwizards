af88bfd1310e7cf4971cdaa8b78aaa31fef67d6e
gatperdut <carlos.romero.breto@gmail.com>
2026-05-02 07:02:35 +0200

    add ws listening to town, too

--- hwfe/src/app/campaigns/campaign/town/town-membership/town-membership.component.ts
+++ hwfe/src/app/campaigns/campaign/town/town-membership/town-membership.component.ts
@@ -1,11 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  DestroyRef,
-  inject,
-  input,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core';
 import { KlassesService } from '@hw/hwfe/app/characters/services/klasses.service';
 import { WhoComponent } from '@hw/hwfe/app/shared/who/who.component';
 import {
@@ -13,16 +6,7 @@ import {
   AppCardMiniAction,
   CardComponent,
 } from '@hw/hwfe/app/ui/card/card.component';
-import { SocketService } from '@hw/hwfe/sockets/socket.service';
-import {
-  CampaignsListDownstream,
-  CampaignsListUpstream,
-  HwCharacter,
-  HwMembership,
-  MembershipsDownstream,
-  MembershipsUpstream,
-} from '@hw/shared';
-import { Socket } from 'socket.io-client';
+import { HwCharacter, HwMembership } from '@hw/shared';
 
 @Component({
   selector: 'app-town-membership',
@@ -33,28 +17,17 @@ import { Socket } from 'socket.io-client';
 })
 export class TownMembershipComponent {
   public klassesService = inject(KlassesService);
-  private socketService = inject(SocketService);
-  private destroyRef = inject(DestroyRef);
 
   public membership = input.required<HwMembership>();
 
   public character = computed(() => this.membership().character as HwCharacter);
 
-  private campaignsSocket!: Socket<CampaignsListDownstream, CampaignsListUpstream>;
-  private membershipsSocket!: Socket<MembershipsDownstream, MembershipsUpstream>;
-
-  constructor() {
-    this.campaignsSocket = this.socketService.socket('campaigns', this.destroyRef);
-    this.membershipsSocket = this.socketService.socket('memberships', this.destroyRef);
-
-    // this.campaignsListen();
-    // this.membershipsListen();
-  }
-
   public actions = computed(() => {
     const result: AppCardAction[] = [];
 
-    result.push(this.buyAction());
+    if (this.membership().me) {
+      result.push(this.buyAction());
+    }
 
     return result;
   });
