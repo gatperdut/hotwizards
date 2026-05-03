d8ace93d8232f0fc6e5e6ab1e4e61a8b57c42c8c
gatperdut <carlos.romero.breto@gmail.com>
2026-05-03 16:21:20 +0200

    add a few pushnots for testing in prod

--- hwbe/src/campaigns/campaigns.module.ts
+++ hwbe/src/campaigns/campaigns.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module.js';
 import { MembershipsModule } from '../memberships/memberships.module.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
+import { PushModule } from '../push/push.module.js';
 import { CampaignsController } from './campaigns.controller.js';
 import { CampaignsGateway } from './campaigns.gateway.js';
 import { CampaignsService } from './campaigns.service.js';
@@ -9,7 +10,7 @@ import { CampaignsService } from './campaigns.service.js';
 @Module({
   controllers: [CampaignsController],
   providers: [CampaignsService, CampaignsGateway],
-  imports: [PrismaModule, AuthModule, MembershipsModule],
+  imports: [PrismaModule, AuthModule, MembershipsModule, PushModule],
   exports: [],
 })
 export class CampaignsModule {}
