9ed1b7c9eb646aa146ddeb3db770e418eeedf6d5
gatperdut <carlos.romero.breto@gmail.com>
2026-05-05 16:02:30 +0200

    more stuff

--- hwfe/src/app/shared/user-menu/user-menu.component.ts
+++ hwfe/src/app/shared/user-menu/user-menu.component.ts
@@ -4,6 +4,7 @@ import {
   PushDialogData,
   PushDialogResult,
 } from '@hw/hwfe/push/push-dialog/push-dialog.component';
+import { AuthService } from '../../auth/services/auth.service';
 import { DialogService, LazyDialog } from '../../ui/dialog/services/dialog.service';
 import { AppMenuItem, MenuComponent } from '../../ui/menu/menu.component';
 
@@ -16,10 +17,16 @@ import { AppMenuItem, MenuComponent } from '../../ui/menu/menu.component';
 })
 export class UserMenuComponent {
   private dialogService = inject(DialogService);
+  private authService = inject(AuthService);
 
   public items: AppMenuItem[] = [
     { label: 'Push notifications', icon: 'user', callback: () => this.pushNotifications() },
-    { label: 'Logout', icon: 'arrow-left-start-on-rectangle', callback: open, color: 'warning' },
+    {
+      label: 'Logout',
+      icon: 'arrow-left-start-on-rectangle',
+      callback: () => this.authService.logout(),
+      color: 'warning',
+    },
   ];
 
   public pushNotifications(): void {
