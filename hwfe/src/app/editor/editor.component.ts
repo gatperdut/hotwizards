aa7627d9af99374dca4c8de3f8562381b46fc46c
gatperdut <carlos.romero.breto@gmail.com>
2026-05-06 15:58:27 +0200

    coords

--- hwfe/src/app/editor/editor.component.ts
+++ hwfe/src/app/editor/editor.component.ts
@@ -9,7 +9,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { Viewport } from 'pixi-viewport';
-import { Application, Assets, Graphics, Sprite, Text, TextStyle } from 'pixi.js';
+import { Application, Assets, Graphics, Point, Sprite, Text, TextStyle } from 'pixi.js';
 import { from, Observable, tap } from 'rxjs';
 import { OverflowService } from '../shared/overflow.service';
 
@@ -29,6 +29,8 @@ export class EditorComponent implements OnInit, AfterViewInit, OnDestroy {
   private viewport!: Viewport;
   private window = this.document.defaultView!;
 
+  private tileW = 64;
+  private tileH = 32;
   private cols = 10;
   private rows = 10;
 
@@ -73,50 +75,62 @@ export class EditorComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   private async draw(): Promise<void> {
-    const texture = await Assets.load('/tiles/ground_1.png');
+    const texture1 = await Assets.load('/tiles/ground/ground_01.png');
+    const texture2 = await Assets.load('/tiles/ground/ground_02.png');
+    const texture3 = await Assets.load('/tiles/ground/ground_03.png');
+    const texture4 = await Assets.load('/tiles/ground/ground_04.png');
 
-    const sprite = new Sprite(texture);
-    sprite.zIndex = 0;
-    sprite.position.set(32, -16);
-
-    const sprite2 = new Sprite(texture);
-    sprite2.zIndex = -1;
-
+    const sprite = new Sprite(texture1);
+    sprite.zIndex = this.groundZIndex(5, 2);
+    sprite.position.copyFrom(this.world2Ground(5, 2));
     sprite.setSize(64, 64);
-    sprite2.setSize(64, 64);
     this.viewport.addChild(sprite);
-    // this.viewport.addChild(sprite2);
-    sprite2.x += 32;
-    sprite2.y -= 16;
+
+    const sprite2 = new Sprite(texture2);
+    sprite2.zIndex = this.groundZIndex(6, 2);
+    sprite2.position.copyFrom(this.world2Ground(6, 2));
+    sprite2.setSize(64, 64);
+    this.viewport.addChild(sprite2);
+
+    const sprite3 = new Sprite(texture3);
+    sprite3.zIndex = this.groundZIndex(5, 3);
+    sprite3.position.copyFrom(this.world2Ground(5, 3));
+    sprite3.setSize(64, 64);
+    this.viewport.addChild(sprite3);
+
+    const sprite4 = new Sprite(texture4);
+    sprite4.zIndex = this.groundZIndex(6, 3);
+    sprite4.position.copyFrom(this.world2Ground(6, 3));
+    sprite4.setSize(64, 64);
+    this.viewport.addChild(sprite4);
 
     this.drawGrid();
     this.drawCoordinates();
   }
 
   private drawGrid(): void {
-    const tileW = 64;
-    const tileH = 32;
     const cols = this.cols;
     const rows = this.rows;
 
     const grid = new Graphics();
+    grid.zIndex = -1;
     grid.setStrokeStyle({ color: 0x444444, pixelLine: true });
 
     // going east
     for (let row = 0; row <= rows; row++) {
-      const startX = (0 + row) * (tileW / 2);
-      const startY = (0 + row) * (tileH / 2);
-      const endX = (rows + row) * (tileW / 2);
-      const endY = (rows - row) * (tileH / 2);
+      const startX = (0 + row) * (this.tileW / 2);
+      const startY = (0 + row) * (this.tileH / 2);
+      const endX = (rows + row) * (this.tileW / 2);
+      const endY = -(rows - row) * (this.tileH / 2);
       grid.moveTo(startX, startY).lineTo(endX, endY);
     }
 
     // going south
     for (let col = 0; col <= cols; col++) {
-      const startX = (0 + col) * (tileW / 2);
-      const startY = (0 - col) * (tileH / 2);
-      const endX = (cols + col) * (tileW / 2);
-      const endY = (cols - col) * (tileH / 2);
+      const startX = (0 + col) * (this.tileW / 2);
+      const startY = (0 - col) * (this.tileH / 2);
+      const endX = (cols + col) * (this.tileW / 2);
+      const endY = (cols - col) * (this.tileH / 2);
       grid.moveTo(startX, startY).lineTo(endX, endY);
     }
 
@@ -125,26 +139,36 @@ export class EditorComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   private drawCoordinates(): void {
-    const tileW = 64;
-    const tileH = 32;
-
     const style = new TextStyle({
       fontSize: 8,
       fill: 0x444444,
     });
 
-    for (let row = 0; row < this.rows; row++) {
-      for (let col = 0; col < this.cols; col++) {
-        const x = (row - col) * (tileW / 2);
-        const y = (row + col) * (tileH / 2);
-
-        const label = new Text({ text: `${row},${col}`, style });
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        const label = new Text({ text: `${x},${y}`, style });
+        label.zIndex = -1;
         label.anchor.set(0.5, 0.5);
-        label.x = x;
-        label.y = y + tileH / 2;
+        label.position.copyFrom(this.world2Screen(x, y));
         this.viewport.addChild(label);
       }
     }
+  }
+
+  private world2Screen(xWorld: number, yWorld: number): Point {
+    const xScreen = (yWorld + xWorld) * (this.tileW / 2);
+    const yScreen = (yWorld - xWorld) * (this.tileH / 2);
+
+    return new Point(xScreen, yScreen);
+  }
+
+  private world2Ground(xWorld: number, yWorld: number): Point {
+    const world = this.world2Screen(xWorld, yWorld);
+    return new Point(world.x, world.y - this.tileH * 1.5);
+  }
+
+  private groundZIndex(xWorld: number, yWorld: number): number {
+    return this.cols - 1 - xWorld + yWorld;
   }
 
   private onResize = (): void => {
