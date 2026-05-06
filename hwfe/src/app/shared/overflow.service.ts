f886e955a1de121a772021c77438d9a964830c4c
gatperdut <carlos.romero.breto@gmail.com>
2026-05-06 06:38:57 +0200

    coordinates -_-

--- hwfe/src/app/shared/overflow.service.ts
+++ hwfe/src/app/shared/overflow.service.ts
@@ -4,13 +4,15 @@ import { DOCUMENT, inject, Injectable } from '@angular/core';
 export class OverflowService {
   private document = inject(DOCUMENT);
 
+  private htmlElement = this.document.documentElement;
+
   public unhide(): void {
-    this.document.documentElement.classList.remove('overflow-hidden');
-    this.document.documentElement.classList.add('scrollbar-gutter-stable');
+    this.htmlElement.classList.remove('overflow-hidden');
+    this.htmlElement.classList.add('scrollbar-gutter-stable');
   }
 
   public hide(): void {
-    this.document.documentElement.classList.add('overflow-hidden');
-    this.document.documentElement.classList.remove('scrollbar-gutter-stable');
+    this.htmlElement.classList.add('overflow-hidden');
+    this.htmlElement.classList.remove('scrollbar-gutter-stable');
   }
 }
