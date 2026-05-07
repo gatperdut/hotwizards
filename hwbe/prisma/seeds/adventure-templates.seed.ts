e3cdd594a7591a4a0168472a467d003b1c928e38
gatperdut <carlos.romero.breto@gmail.com>
2026-05-07 06:19:43 +0200

    start working on the part for selecting the adventure to edit

--- hwbe/prisma/seeds/adventure-templates.seed.ts
+++ hwbe/prisma/seeds/adventure-templates.seed.ts
@@ -2,6 +2,9 @@ import { PrismaClient } from '@hw/prismagen/client';
 
 export async function seedAdventureTemplates(prismaClient: PrismaClient): Promise<void> {
   await prismaClient.adventureTemplate.createMany({
-    data: [{ name: 'The Sunken Tomb' }, { name: 'Raid on Blackmoor Keep' }],
+    data: [
+      { name: 'The Sunken Tomb', map: { width: 10, height: 8, cells: [] } },
+      { name: 'Raid on Blackmoor Keep' },
+    ],
   });
 }
