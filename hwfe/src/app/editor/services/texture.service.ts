856e952d12ae6c5ad8b0057f29513c8e589aec96
gatperdut <carlos.romero.breto@gmail.com>
2026-05-08 18:03:11 +0200

    short detour for the dialogs

--- hwfe/src/app/editor/services/texture.service.ts
+++ hwfe/src/app/editor/services/texture.service.ts
@@ -1,40 +1,25 @@
 import { Injectable } from '@angular/core';
+import { GroundSpritePaths } from '@hw/shared';
 import { Assets, Texture } from 'pixi.js';
-import { from, Observable, tap } from 'rxjs';
+import { forkJoin, from, Observable, tap } from 'rxjs';
 
 @Injectable()
 export class TextureService {
-  private texturesPaths: string[] = [];
   public textures: Record<string, Texture> = {};
 
-  public grounds = [
-    '/tiles/ground/ground_01.png',
-    '/tiles/ground/ground_02.png',
-    '/tiles/ground/ground_03.png',
-    '/tiles/ground/ground_04.png',
-    '/tiles/ground/ground_05.png',
-    '/tiles/ground/ground_06.png',
-    '/tiles/ground/ground_07.png',
-    '/tiles/ground/ground_08.png',
-    '/tiles/ground/ground_09.png',
-    '/tiles/ground/ground_10.png',
-    '/tiles/ground/ground_11.png',
-    '/tiles/ground/ground_12.png',
-  ] as const;
-
-  public load(path: string): Observable<Texture> {
-    if (!this.texturesPaths.includes(path)) {
-      this.texturesPaths.push(path);
-    }
-
-    return from(Assets.load<Texture>(path)).pipe(
-      tap((texture) => {
-        this.textures[path] = texture;
-      }),
+  public setup(): Observable<Texture[]> {
+    return forkJoin(
+      [...GroundSpritePaths].map((spritePath) =>
+        from(Assets.load<Texture>(spritePath)).pipe(
+          tap((texture) => {
+            this.textures[spritePath] = texture;
+          }),
+        ),
+      ),
     );
   }
 
   public shutdown(): void {
-    void Assets.unload(this.texturesPaths);
+    void Assets.unload([...GroundSpritePaths]);
   }
 }
