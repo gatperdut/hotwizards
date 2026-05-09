117622b58afe265efc1c6c88de487f010919a3eb
gatperdut <carlos.romero.breto@gmail.com>
2026-05-09 08:29:34 +0200

    add the feature files

---
+++ hwfe/src/app/editor/types/feature-sprite-paths.const.ts
@@ -0,0 +1,20 @@
+export const FeatureSpritePaths = [
+  '/tiles/feature/barrel.png',
+  '/tiles/feature/chair_e.png',
+  '/tiles/feature/chair_n.png',
+  '/tiles/feature/chair_s_04.png',
+  '/tiles/feature/chair-w.png',
+  '/tiles/feature/fire_shrine.png',
+  '/tiles/feature/plinth.png',
+  '/tiles/feature/throne_e.png',
+  '/tiles/feature/throne_n.png',
+  '/tiles/feature/throne_s.png',
+  '/tiles/feature/throne_w.png',
+  '/tiles/feature/tombstone_x.png',
+  '/tiles/feature/tombstone_y.png',
+  '/tiles/feature/vase.png',
+  '/tiles/feature/water_shrine.png',
+  '/tiles/feature/well.png',
+] as const;
+
+export type FeatureSpritePath = (typeof FeatureSpritePaths)[number];
