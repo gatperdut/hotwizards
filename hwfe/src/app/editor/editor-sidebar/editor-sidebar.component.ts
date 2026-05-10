fb1b659ef5d98ab55be74c0e6da05a8f529b6ec0
gatperdut <carlos.romero.breto@gmail.com>
2026-05-10 08:01:20 +0200

    editor-sidebar boilerplate

---
+++ hwfe/src/app/editor/editor-sidebar/editor-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-sidebar',
+  imports: [],
+  templateUrl: './editor-sidebar.component.html',
+  styleUrl: './editor-sidebar.component.css',
+  providers: [],
+})
+export class EditorSidebarComponent {}
