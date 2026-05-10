26043ce4abdcb7fb514c978c51d8a2c9ed904949
gatperdut <carlos.romero.breto@gmail.com>
2026-05-10 09:07:08 +0200

    work on the sidebar

--- hwfe/src/app/editor/editor-sidebar/editor-sidebar.component.ts
+++ hwfe/src/app/editor/editor-sidebar/editor-sidebar.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import {
+  SidebarButtonAction,
+  SidebarButtonComponent,
+} from './sidebar-button/sidebar-button.component';
+
+export type EditorSidebarButton = {
+  icon: string;
+  callback?: () => void;
+  actions?: SidebarButtonAction[];
+};
 
 @Component({
   selector: 'app-editor-sidebar',
-  imports: [],
+  imports: [SidebarButtonComponent],
   templateUrl: './editor-sidebar.component.html',
   styleUrl: './editor-sidebar.component.css',
   providers: [],
 })
-export class EditorSidebarComponent {}
+export class EditorSidebarComponent {
+  public sidebarButtons: EditorSidebarButton[] = [
+    {
+      icon: 'arrow-uturn-left',
+      callback: (): void => {
+        console.log('back');
+      },
+    },
+    {
+      icon: 'arrow-down-tray',
+      actions: [
+        {
+          icon: 'arrow-uturn-left',
+          callback: (): void => {
+            console.log('chevron down');
+          },
+        },
+        {
+          icon: 'arrow-uturn-left',
+          callback: (): void => {
+            console.log('chevron up');
+          },
+        },
+      ],
+    },
+  ];
+}
---
+++ hwfe/src/app/editor/editor-sidebar/sidebar-button/sidebar-button.component.ts
@@ -0,0 +1,45 @@
+import { Component, ElementRef, HostListener, inject, input } from '@angular/core';
+import { ButtonComponent } from '@hw/hwfe/app/ui/button/button.component';
+
+export interface SidebarButtonAction {
+  icon: string;
+  callback: () => void;
+}
+
+@Component({
+  selector: 'app-sidebar-button',
+  imports: [ButtonComponent],
+  templateUrl: './sidebar-button.component.html',
+  styleUrl: './sidebar-button.component.css',
+  providers: [],
+})
+export class SidebarButtonComponent {
+  private readonly elementRef = inject(ElementRef);
+
+  public icon = input.required<string>();
+  public callback = input<() => void>();
+  public actions = input<SidebarButtonAction[]>([]);
+
+  public expanded = false;
+
+  public onTrigger(): void {
+    const callback = this.callback();
+    if (callback) {
+      callback();
+    } else {
+      this.expanded = !this.expanded;
+    }
+  }
+
+  public onAction(action: SidebarButtonAction): void {
+    action.callback();
+    this.expanded = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  public onClickOutside(event: Event): void {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.expanded = false;
+    }
+  }
+}
