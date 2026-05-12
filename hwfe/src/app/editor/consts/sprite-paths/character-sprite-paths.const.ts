c7face767daaa9dc12e2a6c33150e50ca4c63fed
gatperdut <carlos.romero.breto@gmail.com>
2026-05-12 20:12:22 +0200

    elf and wizard ,male

--- hwfe/src/app/editor/consts/sprite-paths/character-sprite-paths.const.ts
+++ hwfe/src/app/editor/consts/sprite-paths/character-sprite-paths.const.ts
@@ -7,6 +7,14 @@ export const CharacterSpritePaths = [
   '/tiles/characters/dwarf_male_e.png',
   '/tiles/characters/dwarf_male_s.png',
   '/tiles/characters/dwarf_male_w.png',
+  '/tiles/characters/elf_male_n.png',
+  '/tiles/characters/elf_male_e.png',
+  '/tiles/characters/elf_male_s.png',
+  '/tiles/characters/elf_male_w.png',
+  '/tiles/characters/wizard_male_n.png',
+  '/tiles/characters/wizard_male_e.png',
+  '/tiles/characters/wizard_male_s.png',
+  '/tiles/characters/wizard_male_w.png',
 ] as const;
 
 export type CharacterSpritePaths = (typeof CharacterSpritePaths)[number];
