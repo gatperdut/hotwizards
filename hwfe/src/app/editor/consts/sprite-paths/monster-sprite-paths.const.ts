423148d817536d73015ccec5485b005e988ab85f
gatperdut <carlos.romero.breto@gmail.com>
2026-05-13 06:36:02 +0200

    more mosnters

--- hwfe/src/app/editor/consts/sprite-paths/monster-sprite-paths.const.ts
+++ hwfe/src/app/editor/consts/sprite-paths/monster-sprite-paths.const.ts
@@ -1,6 +1,14 @@
 import { Direction } from '@hw/shared';
 
 export const MonsterSpritePaths = [
+  '/tiles/monsters/fimir_n.png',
+  '/tiles/monsters/fimir_e.png',
+  '/tiles/monsters/fimir_s.png',
+  '/tiles/monsters/fimir_w.png',
+  '/tiles/monsters/gargoyle_n.png',
+  '/tiles/monsters/gargoyle_e.png',
+  '/tiles/monsters/gargoyle_s.png',
+  '/tiles/monsters/gargoyle_w.png',
   '/tiles/monsters/goblin_axe_n.png',
   '/tiles/monsters/goblin_axe_e.png',
   '/tiles/monsters/goblin_axe_s.png',
@@ -13,11 +21,27 @@ export const MonsterSpritePaths = [
   '/tiles/monsters/goblin_sword_e.png',
   '/tiles/monsters/goblin_sword_s.png',
   '/tiles/monsters/goblin_sword_w.png',
+  '/tiles/monsters/orc_cleaver_n.png',
+  '/tiles/monsters/orc_cleaver_e.png',
+  '/tiles/monsters/orc_cleaver_s.png',
+  '/tiles/monsters/orc_cleaver_w.png',
+  '/tiles/monsters/skeleton_n.png',
+  '/tiles/monsters/skeleton_e.png',
+  '/tiles/monsters/skeleton_s.png',
+  '/tiles/monsters/skeleton_w.png',
 ] as const;
 
 export type MonsterSpritePath = (typeof MonsterSpritePaths)[number];
 
-export const MonsterTypes = ['goblin_axe', 'goblin_dagger', 'goblin_sword'] as const;
+export const MonsterTypes = [
+  'fimir',
+  'gargoyle',
+  'goblin_axe',
+  'goblin_dagger',
+  'goblin_sword',
+  'orc_cleaver',
+  'skeleton',
+] as const;
 
 export type MonsterType = (typeof MonsterTypes)[number];
 
