6387e8070c19fd3d7b6215cc77ffb54f33ca95de
gatperdut <carlos.romero.breto@gmail.com>
2026-05-17 08:36:59 +0200

    heroes

--- shared/src/sprites/index.ts
+++ shared/src/sprites/index.ts
@@ -5,5 +5,6 @@ export {
   FeatureSpriteSecondaries,
 } from './feature-sprites.const.js';
 export { FloorSpritePath, FloorSpritePaths } from './floor-sprites.const.js';
+export { HeroSpritePath, HeroSpritePaths } from './hero-sprites.const.js';
 export { SpriteOffset } from './sprite-offset.const.js';
 export { WaterSpritePath, WaterSpritePaths } from './water-sprites.const.js';
