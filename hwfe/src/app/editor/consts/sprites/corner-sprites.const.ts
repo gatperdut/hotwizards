7536c5ee1bd11f9936e3c63240345c0af6d40c6d
gatperdut <carlos.romero.breto@gmail.com>
2026-05-19 16:08:17 +0200

    finish corners

--- hwfe/src/app/editor/consts/sprites/corner-sprites.const.ts
+++ hwfe/src/app/editor/consts/sprites/corner-sprites.const.ts
@@ -1,15 +1,15 @@
 import { CornerSpritePath, SpriteOffset } from '@hw/shared/sprites';
 
 export const CornerSpriteSizes: Record<CornerSpritePath, SpriteOffset> = {
-  '/tiles/corners/corner_n.png': { x: 128, y: 32 },
-  '/tiles/corners/corner_e.png': { x: 128, y: 32 },
-  '/tiles/corners/corner_s.png': { x: 128, y: 32 },
-  '/tiles/corners/corner_w.png': { x: 128, y: 32 },
+  '/tiles/corners/corner_n.png': { x: 30, y: 70 },
+  '/tiles/corners/corner_e.png': { x: 30, y: 70 },
+  '/tiles/corners/corner_s.png': { x: 30, y: 70 },
+  '/tiles/corners/corner_w.png': { x: 30, y: 70 },
 } as const;
 
 export const CornerSpriteOffsets: Record<CornerSpritePath, SpriteOffset> = {
-  '/tiles/corners/corner_n.png': { x: 0, y: 0 },
-  '/tiles/corners/corner_e.png': { x: 0, y: 0 },
-  '/tiles/corners/corner_s.png': { x: 0, y: 0 },
-  '/tiles/corners/corner_w.png': { x: 0, y: 0 },
+  '/tiles/corners/corner_n.png': { x: 12, y: -49 },
+  '/tiles/corners/corner_e.png': { x: -13, y: -50 },
+  '/tiles/corners/corner_s.png': { x: 14, y: -49 },
+  '/tiles/corners/corner_w.png': { x: -16, y: -48 },
 } as const;
