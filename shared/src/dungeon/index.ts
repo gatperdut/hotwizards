22e347eab9e86c4f9c9fe6692a56c6b277aec2f9
gatperdut <carlos.romero.breto@gmail.com>
2026-05-21 06:18:44 +0200

    very little

--- shared/src/dungeon/index.ts
+++ shared/src/dungeon/index.ts
@@ -1,2 +1,4 @@
 export { HwCell } from './cell.interface.js';
 export { HwDungeon } from './dungeon.interface.js';
+export { MonsterNames } from './monster-names.const.js';
+export { HwMonster } from './monster.interface.js';
