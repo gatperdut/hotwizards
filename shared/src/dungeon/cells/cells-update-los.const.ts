93558d2c2ccc1c68ec490e6035891fa1bad8ab88
gatperdut <carlos.romero.breto@gmail.com>
2026-06-02 07:59:07 +0200

    deal with visibility of features

--- shared/src/dungeon/cells/cells-update-los.const.ts
+++ shared/src/dungeon/cells/cells-update-los.const.ts
@@ -1,8 +1,16 @@
+import { cellAt } from './cell-at.const.js';
 import { cellLos } from './cell-los.const.js';
 import { HwCell } from './cell.interface.js';
+import { sameCell } from './same-cell.const.js';
 
 export const cellsUpdateLos = (cells: HwCell[], origins: HwCell[]): void => {
+  const skip: HwCell[] = [];
+
   cells.forEach((cell) => {
+    if (skip.find((c) => sameCell(c, cell))) {
+      return;
+    }
+
     if (cell.visibility === 2) {
       cell.visibility = 1;
     }
@@ -12,6 +20,26 @@ export const cellsUpdateLos = (cells: HwCell[], origins: HwCell[]): void => {
     origins.forEach((origin) => {
       if (cellLos(cells, origin, cell)) {
         cell.visibility = 2;
+
+        let featureCell: HwCell | undefined;
+
+        if (cell.secondary) {
+          featureCell = cellAt(cells, cell.secondary.x, cell.secondary.y);
+        } else if (cells.find((c) => c.secondary && sameCell(cell, c.secondary))) {
+          featureCell = cell;
+        }
+
+        if (!featureCell) {
+          return;
+        }
+
+        [
+          featureCell,
+          ...cells.filter((c) => c.secondary && sameCell(featureCell, c.secondary)),
+        ].forEach((c) => {
+          c.visibility = 2;
+          skip.push(c);
+        });
       }
     });
   });
