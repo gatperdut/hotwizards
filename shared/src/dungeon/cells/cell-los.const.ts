244989c14e3e9265290cb93c89749b36a21b2f03
gatperdut <carlos.romero.breto@gmail.com>
2026-06-02 07:48:53 +0200

    better algorithm

--- shared/src/dungeon/cells/cell-los.const.ts
+++ shared/src/dungeon/cells/cell-los.const.ts
@@ -6,57 +6,43 @@ export const cellLos = (cells: HwCell[], origin: HwCell, dest: HwCell): boolean 
     return true;
   }
 
-  const dx = Math.abs(dest.x - origin.x);
-  const dy = Math.abs(dest.y - origin.y);
-  const sx = origin.x < dest.x ? 1 : -1;
-  const sy = origin.y < dest.y ? 1 : -1;
+  const dx = dest.x - origin.x;
+  const dy = dest.y - origin.y;
+  const sx = dx > 0 ? 1 : -1;
+  const sy = dy > 0 ? 1 : -1;
+  const adx = Math.abs(dx);
+  const ady = Math.abs(dy);
 
   let x = origin.x;
   let y = origin.y;
-  let err = dx - dy;
 
-  const e2start = 2 * err;
-  if (e2start > 0) {
-    x += sx;
-    err -= dy;
-  } else if (e2start < 0) {
-    y += sy;
-    err += dx;
-  } else {
-    x += sx;
-    y += sy;
-    err += dx - dy;
-  }
+  let tMaxX = ady;
+  let tMaxY = adx;
+  const tDeltaX = 2 * ady;
+  const tDeltaY = 2 * adx;
 
   while (true) {
+    if (tMaxX < tMaxY) {
+      x += sx;
+      tMaxX += tDeltaX;
+    } else if (tMaxX > tMaxY) {
+      y += sy;
+      tMaxY += tDeltaY;
+    } else {
+      if (!cellAt(cells, x + sx, y) || !cellAt(cells, x, y + sy)) {
+        return false;
+      }
+      x += sx;
+      y += sy;
+      tMaxX += tDeltaX;
+      tMaxY += tDeltaY;
+    }
+
     if (x === dest.x && y === dest.y) {
       return true;
     }
-
     if (!cellAt(cells, x, y)) {
       return false;
-    }
-
-    const e2 = 2 * err;
-
-    if (e2 === 0) {
-      const cell1 = cellAt(cells, x + sx, y);
-      const cell2 = cellAt(cells, x, y + sy);
-      if (!cell1 || !cell2) {
-        return false;
-      }
-
-      x += sx;
-      y += sy;
-      err += dx - dy;
-    } else {
-      if (e2 > 0) {
-        x += sx;
-        err -= dy;
-      } else {
-        y += sy;
-        err += dx;
-      }
     }
   }
 };
